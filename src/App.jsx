d8f67214da9784874fe675dfb20754706de7186c
Eriy5819 <eriy5819.com>
2023-03-28 16:00:22 +0700

    Init Project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex justify-center'>
+      <h1 className='text-3xl text-blue-500 font-bold'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
